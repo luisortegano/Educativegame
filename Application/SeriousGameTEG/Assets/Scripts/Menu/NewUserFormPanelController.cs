9631ec217dd1481d22cefccd96fde6536930729e
Luis Ortegano <luis.ortegano.mora@gmail.com>
2015-12-01 20:12:23 -0430

    Cambios en Main2
    Cambios en Main2

--- Application/SeriousGameTEG/Assets/Scripts/Menu/NewUserFormPanelController.cs
+++ Application/SeriousGameTEG/Assets/Scripts/Menu/NewUserFormPanelController.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.IO;
 
 public class NewUserFormPanelController : MonoBehaviour {
 
 	public GameObject UserSelectionPanel;
+
+	public InputField nameInputField;
+	public InputField lastNameInputField;
+	public InputField supeUserInputField;
+	public Toggle isSuperUserToggle;
 
 	public void CancelCreateNewUser () {
 		this.gameObject.SetActive(false);
@@ -15,7 +21,13 @@ public class NewUserFormPanelController : MonoBehaviour {
 		this.gameObject.SetActive(false);
 		//insert into user (name, lastname) values ('no name','no last name');
 		SqliteDatabase sql = new SqliteDatabase (Path.Combine (Application.persistentDataPath, "TEG_SG.db"));
-		sql.ExecuteNonQuery("insert into user (name, lastname) values ('no name','no last name');");
+
+		sql.ExecuteNonQuery("insert into user (name, lastname) values ('"+
+		                    this.nameInputField.text.Trim()+"','"+
+		                    this.lastNameInputField.text.Trim()+"');");
+		if(this.isSuperUserToggle.isOn){
+
+		}
 
 		/*Guardar imagen en carpeta con el numero de id del usuario */
 
