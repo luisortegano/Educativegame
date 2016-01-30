176fded37195eee4f36d49e3e4410f9fa2771fb7
Luis Ortegano <luis.ortegano.mora@gmail.com>
2016-01-30 12:49:29 -0430

    Elementos de scroll de categoria
    Todos los elementos archivos y codigo desarrollado para el scroll de
    categoria de juegos

---
+++ Application/SeriousGameTEG/Assets/Prefab/Category/CategoryPanel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class CategoryPanel : MonoBehaviour {
+	public Text testText;
+	//public Button button;
+	//public Text nameLabel;
+	//public Text lastNameLabel;
+}
---
+++ Application/SeriousGameTEG/Assets/Scripts/Menu/CategoryContentPanel.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CategoryContentPanel : MonoBehaviour {
+
+	public GameObject categoryPanelContainer;
+
+	void OnBecameVisible (){
+		GameObject[] ListUsers = GameObject.FindGameObjectsWithTag("CategoryPanel");
+		this.cleanCategoryContentPanel(ListUsers);
+		//this.populateUsers();
+	}
+
+	public void cleanCategoryContentPanel(GameObject[] ListCategory){
+		if(ListCategory!=null && 0 < ListCategory.Length ){
+			foreach(GameObject current in ListCategory){
+				DestroyImmediate(current.gameObject);
+			}
+		}
+	}
+
+	public void populateCategory(){
+		for ( int i = 0 ; i < 4 ; i++ ){
+			this.instantiateCategory();
+		}
+
+		/*
+		foreach( DataRow currentUser in Usuarios.Users.Rows ){
+			this.InstantiateUser(Convert.ToString(currentUser[UserSQLite.Name]),Convert.ToString(currentUser[UserSQLite.LastName]), Convert.ToInt32(currentUser[UserSQLite.Id]));
+		}
+		*/
+	}
+
+
+	public void instantiateCategory () {
+		GameObject newCategoryPanel = (GameObject)Instantiate(categoryPanelContainer); 
+		CategoryPanel panel = newCategoryPanel.GetComponent<CategoryPanel>();
+		panel.testText.text = "prueba";
+		newCategoryPanel.transform.SetParent(gameObject.transform);
+		newCategoryPanel.transform.localScale = new Vector3(1,1,1);
+	}
+}
