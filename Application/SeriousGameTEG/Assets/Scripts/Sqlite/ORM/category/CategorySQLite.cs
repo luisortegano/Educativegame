7331e0d2867c4277d80abc9dfee5e7b506589b55
Luis Ortegano <luis.ortegano.mora@gmail.com>
2016-02-14 12:33:31 -0430

    Categoria de Juegos
    Cracion de manejador de categorias para sqlite

---
+++ Application/SeriousGameTEG/Assets/Scripts/Sqlite/ORM/category/CategorySQLite.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ORM
+{
+	public class CategorySQLite{
+		public static string table = "category";
+		public static string Id = "id";
+		public static string Name = "name";
+		public static string Description = "description";
+	}
+}
+
