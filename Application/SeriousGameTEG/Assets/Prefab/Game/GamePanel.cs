e4a7e308f10817e8c1cc3b468701a434c4838718
Luis Ortegano <luis.ortegano.mora@gmail.com>
2016-02-14 12:31:56 -0430

    Improve de Juegos
    Mejora en la muestra y solicutud de los juegos

--- Application/SeriousGameTEG/Assets/Prefab/Game/GamePanel.cs
+++ Application/SeriousGameTEG/Assets/Prefab/Game/GamePanel.cs
@@ -4,6 +4,12 @@ using System.Collections;
 
 
 public class GamePanel : MonoBehaviour {
+	public int GameId;
 	public Text NameText;
 	public Text CategoriaText;
+	public Button SelectLevelButton;
+
+	public void selectGameButtonAction (){
+		Debug.Log ( "The game " + this.NameText.text + " with id: " + this.GameId + " was selected");
+	}
 }
