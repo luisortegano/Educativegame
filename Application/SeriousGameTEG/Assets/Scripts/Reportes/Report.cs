e4170ad576c6143202c9db56925d1254ca89b1cf
Luis Ortegano <luis.ortegano.mora@gmail.com>
2016-07-13 08:20:14 -0400

    25 - Clase base de reportes

---
+++ Application/SeriousGameTEG/Assets/Scripts/Reportes/Report.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/**
+Base Class for report objects 
+
+virtual methos are dospised for basic behavior
+**/
+
+public abstract class Report {
+	private int Id;
+	private string Name;
+	private string Description;
+	private string Prefab;
+}
