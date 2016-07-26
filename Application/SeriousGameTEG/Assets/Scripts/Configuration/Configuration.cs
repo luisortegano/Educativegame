59ddd25eb852d3ea8be185b7291a69baabb98118
Luis Ortegano <luis.ortegano.mora@gmail.com>
2016-07-25 22:51:52 -0400

    Avances de Reportes

--- Application/SeriousGameTEG/Assets/Scripts/Configuration/Configuration.cs
+++ Application/SeriousGameTEG/Assets/Scripts/Configuration/Configuration.cs
@@ -6,22 +6,37 @@ public class Configuration : MonoBehaviour {
 	void Awake () {
 		string dbPath = System.IO.Path.Combine (Application.persistentDataPath, "TEG_SG.db");
 		var dbTemplatePath = System.IO.Path.Combine(Application.streamingAssetsPath, "TEG_SG.db");
-        Debug.Log("DataBasePath on StreamingAssets [" + dbTemplatePath + "]");
-        Debug.Log("DataBasePath on Copy [" + dbPath + "]");
-
+        
         if (!System.IO.File.Exists(dbPath)) {
-			Debug.Log ("The Database was not initialized");
+			Debug.Log ("#####The Database was not initialized");
 			// game database does not exists, copy default db as template
 			if (Application.platform == RuntimePlatform.Android)
 			{
 				// Must use WWW for streaming asset
 				WWW reader = new WWW(dbTemplatePath);
 				while ( !reader.isDone) {}
-				Debug.Log ("#########################Copying DataBase From Streaming Assets (Android)");
+				Debug.Log ("#####Copying DataBase From Streaming Assets (Android)");
 				System.IO.File.WriteAllBytes(dbPath, reader.bytes);
 			} else {
-				Debug.Log ("#########################Copying DataBase From Streaming Assets (NO Android)");
+				Debug.Log ("#####Copying DataBase From Streaming Assets (NO Android)");
 				System.IO.File.Copy(dbTemplatePath, dbPath, true);
+			}       
+		}
+
+		// Copy D3js lib
+		if (!System.IO.File.Exists(System.IO.Path.Combine (Application.persistentDataPath, "d3.min.js"))) {
+			Debug.Log ("#####d3.min.js was not in local folder");
+			// game database does not exists, copy default db as template
+			if (Application.platform == RuntimePlatform.Android)
+			{
+				WWW reader = new WWW(System.IO.Path.Combine(Application.streamingAssetsPath, "d3.min.js.file"));
+				while (!reader.isDone) {}
+				Debug.Log ("##### d3.min.js.file -> " + System.IO.Path.Combine(Application.persistentDataPath, "d3.min.js"));
+				System.IO.File.WriteAllBytes(System.IO.Path.Combine (Application.persistentDataPath, "d3.min.js"), reader.bytes);
+			} else {
+				Debug.Log ("#####Copying DataBase From Streaming Assets (NO Android)");
+				System.IO.File.Copy(System.IO.Path.Combine(Application.streamingAssetsPath, "d3.min.js"),
+					System.IO.Path.Combine (Application.persistentDataPath, "d3.min.js"), true);
 			}       
 		}
 	}
