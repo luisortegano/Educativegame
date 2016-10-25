ffd74002b27b7689796c8c391247a273ce603ebd
Luis Ortegano <luis.ortegano.mora@gmail.com>
2016-10-25 13:32:46 -0400

    Ajustes de Helpers de juegos

---
+++ Application/SeriousGameTEG/Assets/Scripts/ReconocimientoFiguras/AnimationHelper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AnimationHelper : MonoBehaviour {
+
+	private Image helperImage;
+	private int amountFig = 4;
+	private int figure = 0;
+
+	void Start () {
+		this.helperImage = this.gameObject.GetComponent<Image>();
+		StartCoroutine(changeImage());
+	}
+
+	public IEnumerator changeImage() {
+		while(true){
+			this.figure = (this.figure+1)%this.amountFig;
+			this.helperImage.overrideSprite = Resources.Load<Sprite>(string.Format("reconocimientoFig/{0}",this.figure));
+			yield return new WaitForSeconds(1.4f);
+		}
+	}
+}
---
+++ Application/SeriousGameTEG/Assets/Scripts/ReconocimientoNumeros/AnimationHelperRN.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AnimationHelperRN : MonoBehaviour {
+
+	private Image helperImage;
+	private int amountFig = 2;
+	private int figure = 0;
+
+	void Start () {
+		this.helperImage = this.gameObject.GetComponent<Image>();
+		StartCoroutine(changeImage());
+	}
+
+	public IEnumerator changeImage() {
+		while(true){
+			this.figure = (this.figure+1)%this.amountFig;
+			this.helperImage.overrideSprite = Resources.Load<Sprite>(string.Format("ordenNumeros/{0}",this.figure));
+			yield return new WaitForSeconds(1.3f);
+		}
+	}
+}
