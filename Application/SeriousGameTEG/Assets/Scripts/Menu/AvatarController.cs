fd676a6d5742ccf41cb1369ac9ae5b0ea15175f3
Luis Ortegano <luis.ortegano.mora@gmail.com>
2016-10-30 20:42:29 -0400

    Eliminar flujo de regreso de seleccion de usuario

--- Application/SeriousGameTEG/Assets/Scripts/Menu/AvatarController.cs
+++ Application/SeriousGameTEG/Assets/Scripts/Menu/AvatarController.cs
@@ -9,7 +9,7 @@ public class AvatarController : MonoBehaviour {
 	public void ToggleUserSelectionPanel(){
 		UserInterfaceManager UIM = UIManager.GetComponent<UserInterfaceManager>();
 
-		if (UIM.MenuActiveSelf (Menu.HomePanel) || UIM.MenuActiveSelf (Menu.UserSelectionPanel)) {
+		if (UIM.MenuActiveSelf (Menu.HomePanel) /*|| UIM.MenuActiveSelf (Menu.UserSelectionPanel)*/) {
 			if( !UIM.MenuActiveSelf(Menu.NewUserFormPanel) ){
 				UIM.MenuSetActive(Menu.UserSelectionPanel,!UIM.MenuActiveSelf(Menu.UserSelectionPanel));
 				UIM.MenuSetActive(Menu.HomePanel,!UIM.MenuActiveSelf(Menu.HomePanel));
